b211dd248b1074487a3ec18d599059ce4aa545b1
Eli Cleveland <elicleveland12@gmail.com>
2018-01-17 01:24:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,6 @@
-var playlist = { prince: "1999" };
+var playlist = { 
+  prince: "1999"
+};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle });
@@ -6,5 +8,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName;
-  playlist; 
+  return playlist; 
 }
